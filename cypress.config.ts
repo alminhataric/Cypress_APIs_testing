8deecdc227ecd86e9a11efbbb4ca3d4bc3bce41a
alminhataric <almin.hataric@edu.fit.ba>
2022-12-12 21:33:48 +0100

    npm_scripts_and_cypress_CLI

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  video: false,
   
   e2e: {
     baseUrl: 'https://localhost:4200',
