312c8c345c0e46b0bb36740101e7ed3c0162d5dc
alminhataric <almin.hataric@edu.fit.ba>
2023-01-16 21:11:08 +0100

    environment_variables_without_last_part

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,11 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  env:{
+    username: 'cytest@test.com',
+    password: 'Welcome123',
+    apiUrl: 'https://api.realworld.io'
+  },
   
   e2e: {
     baseUrl: 'https://localhost:4200',
